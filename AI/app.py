77677ae489716c0802302175c052c6b088fa4e98
B.NAVEEN BHARATHI <144258519+bnaveenbharathi@users.noreply.github.com>
2024-04-12 22:19:39 +0530

    AI update

--- AI/app.py
+++ AI/app.py
@@ -1,26 +1,48 @@
 from flask import Flask, render_template, request
-from dotenv import load_dotenv
-import os
 import google.generativeai as genai
+import nltk
 
-load_dotenv()  # Take environment variables from .env.
+# Configure the API key directly
+api_key = "AIzaSyBVbV0KNNMV70FVebcWm2Vn5AoNlXAvNlA"  # Replace "YOUR_API_KEY_HERE" with your actual API key
+genai.configure(api_key=api_key)
 
-os.getenv("AIzaSyBVbV0KNNMV70FVebcWm2Vn5AoNlXAvNlA")
-genai.configure(api_key=os.getenv("AIzaSyBVbV0KNNMV70FVebcWm2Vn5AoNlXAvNlA"))
-
-#AI connect
+# AI connect
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
+
+nltk.download('punkt')
 
 def get_gemini_response(question):
     response = chat.send_message(question, stream=True)
     return response
 
+def format_response(response_text):
+    # Split response into lines
+    lines = response_text.split('\n')
+
+    formatted_response = ''
+    for line in lines:
+        # Check if the line contains double asterisks
+        if '**' in line:
+            # Split the line by double asterisks
+            segments = line.split('**')
+            # Bolden the segments between double asterisks
+            for i, segment in enumerate(segments):
+                if i % 2 == 1:
+                    segments[i] = f'<b>{segment}</b>'
+            # Join the segments back together
+            line = ''.join(segments)
+        # Add line break after each line
+        formatted_response += f'{line}<br>'
+
+    return formatted_response
+
 app = Flask(__name__)
 title='G-Fluent'
+
 @app.route('/g-ai')
 def index():
-    return render_template('index.html',title=title)
+    return render_template('index.html', title=title)
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
@@ -34,7 +56,9 @@ def ask_question():
     for chunk in response:
         response_text += chunk.text + ' '
 
-    return response_text
+    formatted_response = format_response(response_text)
+
+    return formatted_response
 
 if __name__ == '__main__':
     app.run(debug=True)
